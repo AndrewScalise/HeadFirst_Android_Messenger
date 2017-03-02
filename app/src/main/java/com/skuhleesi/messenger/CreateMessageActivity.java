846fb1ab6454f9e0e3a3c953a3a1a861cca0d803
AndrewScalise <paradigmcomplex@gmail.com>
2017-03-02 14:37:01 -0600

    Send message to receive activity

--- app/src/main/java/com/skuhleesi/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/skuhleesi/messenger/CreateMessageActivity.java
@@ -1,7 +1,10 @@
 package com.skuhleesi.messenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class CreateMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    //call onSendMessage(0 when the button is clicked
+    public void onSendMessage(View view){
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
     }
 }
